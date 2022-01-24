30d92356729609cbdd22c805ab73b26beacfc6f7
JeremyDelorme <jeremydelorme23.7@gmail.com>
2022-01-24 15:51:30 -0500

    created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'Poutine';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 2.04,
+    types: ['grass', 'poison'],
+  },
+  {
+    name: 'Charmander',
+    height: 2.00,
+    types: 'fire',
+  },
+  {
+    name: 'Gengar',
+    height: 4.11,
+    types: ['ghost', 'poison'],
+  },
+];
